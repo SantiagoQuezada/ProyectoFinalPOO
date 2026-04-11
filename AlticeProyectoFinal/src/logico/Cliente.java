4b0d7bb1bd49ac8dc3d11f24160043fab8028812
Ghoosly <sacx0001@ce.pucmm.edu.do>
2026-04-11 12:06:47 -0400

    Creacion de Empleados y arreglo de clientes

--- AlticeProyectoFinal/src/logico/Cliente.java
+++ AlticeProyectoFinal/src/logico/Cliente.java
@@ -1,5 +1,19 @@
 package logico;
 
 public class Cliente {
+    private String idCliente;
+    private String nombre;
+    private Plan plan; // Relación con el plan contratado
 
+    public Cliente(String idCliente, String nombre, Plan plan) {
+        this.idCliente = idCliente;
+        this.nombre = nombre;
+        this.plan = plan;
+    }
+
+    public String getIdCliente() { return idCliente; }
+    public String getNombre() { return nombre; }
+    public Plan getPlan() { return plan; }
+    
+    public void setPlan(Plan plan) { this.plan = plan; }
 }
