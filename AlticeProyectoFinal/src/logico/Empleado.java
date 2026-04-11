a97a6e9f21b6142048ace72915a2ff37caf9b8ea
Ghoosly <sacx0001@ce.pucmm.edu.do>
2026-04-11 12:08:47 -0400

    Creacion de Empleados, arreglo de clientes, y limpieza del programa.

--- AlticeProyectoFinal/src/logico/Empleado.java
+++ AlticeProyectoFinal/src/logico/Empleado.java
@@ -1,5 +1,19 @@
 package logico;
 
 public class Empleado {
+    private String idEmpleado;
+    private String nombre;
+    private String cargo;
 
+    public Empleado(String idEmpleado, String nombre, String cargo) {
+        this.idEmpleado = idEmpleado;
+        this.nombre = nombre;
+        this.cargo = cargo;
+    }
+
+    public String getIdEmpleado() { return idEmpleado; }
+    public String getNombre() { return nombre; }
+    public String getCargo() { return cargo; }
+    
+    public void setCargo(String cargo) { this.cargo = cargo; }
 }
