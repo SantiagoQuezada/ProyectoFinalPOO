cd274a1880801eaf52b6c169fc032e2f5cea91b2
Ghoosly <sacx0001@ce.pucmm.edu.do>
2026-04-12 14:32:58 -0400

    Se le agregaron mas campos a Clientes

--- AlticeProyectoFinal/src/Logico/Cliente.java
+++ AlticeProyectoFinal/src/Logico/Cliente.java
@@ -1,19 +1,63 @@
 package Logico;
 
 public class Cliente {
-    private String idCliente;
-    private String nombre;
-    private Plan plan; // Relación con el plan contratado
+	private String idCliente;
+	private String cedula;
+	private String nombre;
+	private String telefono;
+	private String direccion;
+	private Plan plan;
 
-    public Cliente(String idCliente, String nombre, Plan plan) {
-        this.idCliente = idCliente;
-        this.nombre = nombre;
-        this.plan = plan;
-    }
+	public Cliente(String idCliente, String cedula, String nombre, String telefono, String direccion, Plan plan) {
+		this.idCliente = idCliente;
+		this.cedula = cedula;
+		this.nombre = nombre;
+		this.telefono = telefono;
+		this.direccion = direccion;
+		this.plan = plan;
+	}
 
-    public String getIdCliente() { return idCliente; }
-    public String getNombre() { return nombre; }
-    public Plan getPlan() { return plan; }
-    
-    public void setPlan(Plan plan) { this.plan = plan; }
+	public String getIdCliente() {
+		return idCliente;
+	}
+
+	public String getCedula() {
+		return cedula;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public Plan getPlan() {
+		return plan;
+	}
+
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public void setPlan(Plan plan) {
+		this.plan = plan;
+	}
 }
