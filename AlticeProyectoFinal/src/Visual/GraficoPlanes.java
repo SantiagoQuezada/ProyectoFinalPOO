0e8c9b013f0cbc09be98ee4ca036c369ccf6549b
kendry <chriskendryh@gmail.com>
2026-04-13 17:20:19 -0400

    datos y graficos

--- AlticeProyectoFinal/src/Visual/GraficoPlanes.java
+++ AlticeProyectoFinal/src/Visual/GraficoPlanes.java
@@ -1,42 +1,15 @@
 package Visual;
 
-import java.awt.EventQueue;
+import Logico.Altice;
+import Logico.Cliente;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
+import javax.swing.JDialog;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.util.HashMap;
+import java.util.Map;
 
-public class GraficoPlanes extends JFrame {
+public class GraficoPlanes extends JDialog {
 
-	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					GraficoPlanes frame = new GraficoPlanes();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
-	public GraficoPlanes() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-
-	}
 
 }
