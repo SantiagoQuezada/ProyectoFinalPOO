a2416c9555e652bcb93d030cd74f2d4c46c60254
kendry <chriskendryh@gmail.com>
2026-04-14 10:55:52 -0400

    formato empleado

--- AlticeProyectoFinal/src/Logico/Empleado.java
+++ AlticeProyectoFinal/src/Logico/Empleado.java
@@ -3,68 +3,66 @@ package Logico;
 import java.io.Serializable;
 
 public class Empleado extends Persona implements Serializable {
-    private static final long serialVersionUID = 1L;
-    private String idEmpleado;
-    private String departamento;
-    private float salario;
-    private Usuario usuario;
-    private String estado;
+	private static final long serialVersionUID = 1L;
+	private String idEmpleado;
+	private String departamento;
+	private float salario;
+	private Usuario usuario;
+	private String estado;
 
-    public Empleado(String cedula, String nombre, String telefono, String direccion, String idEmpleado, String departamento, float salario, Usuario usuario, String estado) {
-        super(cedula, nombre, telefono, direccion);
-        this.idEmpleado = idEmpleado;
-        this.departamento = departamento;
-        this.salario = salario;
-        this.usuario = usuario;
-        this.estado = estado;
-    }
+	public Empleado(String cedula, String nombre, String telefono, String direccion, String idEmpleado,
+			String departamento, float salario, Usuario usuario, String estado) {
+		super(cedula, nombre, telefono, direccion);
+		this.idEmpleado = idEmpleado;
+		this.departamento = departamento;
+		this.salario = salario;
+		this.usuario = usuario;
+		this.estado = estado;
+	}
 
-    public String getIdEmpleado() {
-        return idEmpleado;
-    }
+	public String getIdEmpleado() {
+		return idEmpleado;
+	}
 
-    public void setIdEmpleado(String idEmpleado) {
-        this.idEmpleado = idEmpleado;
-    }
+	public void setIdEmpleado(String idEmpleado) {
+		this.idEmpleado = idEmpleado;
+	}
 
-    public String getDepartamento() {
-        return departamento;
-    }
+	public String getDepartamento() {
+		return departamento;
+	}
 
-    public void setDepartamento(String departamento) {
-        this.departamento = departamento;
-    }
+	public void setDepartamento(String departamento) {
+		this.departamento = departamento;
+	}
 
-    public float getSalario() {
-        return salario;
-    }
+	public float getSalario() {
+		return salario;
+	}
 
-    public void setSalario(float salario) {
-        this.salario = salario;
-    }
+	public void setSalario(float salario) {
+		this.salario = salario;
+	}
 
-    public Usuario getUsuario() {
-        return usuario;
-    }
+	public Usuario getUsuario() {
+		return usuario;
+	}
 
-    public void setUsuario(Usuario usuario) {
-        this.usuario = usuario;
-    }
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
 
-    public String getEstado() {
-        return estado;
-    }
+	public String getEstado() {
+		return estado;
+	}
 
-    public void setEstado(String estado) {
-        this.estado = estado;
-    }
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
 
-    @Override
-    public String mostrarDetalles() {
-        return "Detalles del Empleado:\n" +
-               "ID: " + idEmpleado + "\n" +
-               "Nombre: " + getNombre() + "\n" +
-               "Departamento: " + departamento + "\n" +
-               "Estado: " + estado;
-    }
+	@Override
+	public String mostrarDetalles() {
+		return "Detalles del Empleado:\n" + "ID: " + idEmpleado + "\n" + "Nombre: " + getNombre() + "\n"
+				+ "Departamento: " + departamento + "\n" + "Estado: " + estado;
+	}
 }
