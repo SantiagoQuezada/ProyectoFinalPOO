40372d8b40366feeb63c189a2388233d2e8d386c
kendry <chriskendryh@gmail.com>
2026-04-14 10:57:34 -0400

    ultimo formato logico

--- AlticeProyectoFinal/src/Logico/Usuario.java
+++ AlticeProyectoFinal/src/Logico/Usuario.java
@@ -3,38 +3,38 @@ package Logico;
 import java.io.Serializable;
 
 public class Usuario implements Serializable {
-    private static final long serialVersionUID = 1L;
-    private String username;
-    private String password;
-    private Rol rol;
+	private static final long serialVersionUID = 1L;
+	private String username;
+	private String password;
+	private Rol rol;
 
-    public Usuario(String username, String password, Rol rol) {
-        this.username = username;
-        this.password = password;
-        this.rol = rol;
-    }
+	public Usuario(String username, String password, Rol rol) {
+		this.username = username;
+		this.password = password;
+		this.rol = rol;
+	}
 
-    public String getUsername() {
-        return username;
-    }
+	public String getUsername() {
+		return username;
+	}
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
-    public String getPassword() {
-        return password;
-    }
+	public String getPassword() {
+		return password;
+	}
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
-    public Rol getRol() {
-        return rol;
-    }
+	public Rol getRol() {
+		return rol;
+	}
 
-    public void setRol(Rol rol) {
-        this.rol = rol;
-    }
+	public void setRol(Rol rol) {
+		this.rol = rol;
+	}
 }
